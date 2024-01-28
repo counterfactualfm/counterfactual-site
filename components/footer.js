325e7f028f69eb242a8c36ddb17ca7ea84fec730
Taylor Gregoire-Wright <taylor.gregoirewright@outlook.com>
2024-01-28 11:45:42 -0800

    Style header & footer

--- components/footer.js
+++ components/footer.js
@@ -4,19 +4,16 @@ export default function Footer() {
   return (
     <footer className="border-t border-accent-2 bg-accent-1">
       <Container>
-        <div className="flex flex-col items-center py-28 lg:flex-row">
-          <h3 className="mb-10 text-center text-4xl font-semibold leading-tight tracking-tighter text-teal-800 lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
+        <div className="flex flex-col items-center py-28 align-baseline">
+          <h3 className="mb-10 grow text-center text-4xl font-semibold leading-tight tracking-tighter text-teal-600 lg:text-5xl">
             We read together. And it is good.
           </h3>
-          <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-            {" "}
-            <a
-              href="mailto:hello@counterfactual.fm"
-              className="mx-3 font-semibold hover:underline"
-            >
-              hello@counterfactual.fm
-            </a>
-          </div>
+          <a
+            href="mailto:hello@counterfactual.fm"
+            className="mx-3 font-semibold text-teal-800 hover:underline"
+          >
+            hello@counterfactual.fm
+          </a>
         </div>
       </Container>
     </footer>
--- components/intro.js
+++ components/intro.js
@@ -1,9 +1,12 @@
 export default function Intro() {
   return (
-    <section className="mb-16 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
-      <h1 className="text-6xl font-semibold leading-tight tracking-tighter text-teal-800 md:pr-8 md:text-8xl">
+    <section className="mb-16 mt-16 flex flex-col items-center">
+      <h1 className="text-center text-6xl font-semibold leading-tight tracking-tighter text-teal-800 md:pr-8 md:text-8xl">
         Counterfactual
       </h1>
+      <h2 className="text-center text-lg font-semibold leading-tight tracking-tighter text-teal-600 md:pr-8 md:text-3xl">
+        We read together. And it is good.
+      </h2>
     </section>
   );
 }
