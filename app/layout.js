e99e610e528e9c55ac9f2fcf4efe83c599fbb059
Taylor Gregoire-Wright <taylor.gregoirewright@outlook.com>
2024-01-27 21:21:32 -0600

    Remove alert

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,9 @@
 import "./globals.css";
 
-import Alert from "@/components/alert";
 import Footer from "@/components/footer";
 
-import { draftMode } from "next/headers";
 import Container from "@/components/container";
+import { draftMode } from "next/headers";
 
 export default function RootLayout({ children }) {
   const { isEnabled } = draftMode();
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <div className="min-h-screen">
-          <Alert preview={isEnabled} />
           <main>
             <Container>{children}</Container>
           </main>
